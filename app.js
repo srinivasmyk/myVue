515595af87db575eb76c72e46f45f240c66b0ccb
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2022-03-30 10:26:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,19 +57,3 @@ methods:{
 });
 app2.mount('#events');
 
-const app= Vue.createApp({
-data(){
-    return {
-        myName:"srini",
-        myAge:27,
-        myImage: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_1280.jpg",
-        randomNum: Math.random(),
-    }
-},
-methods:{
-
-
-}
-});
-
-app.mount('#assignment');
