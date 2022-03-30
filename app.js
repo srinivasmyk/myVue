3d37d4ef29fc2c36170461c70752bb6654fecf5b
srinivas <srinivas.myakala18@gmail.com>
2022-03-30 10:17:07 +0530

    first comit

---
+++ app.js
@@ -0,0 +1,16 @@
+const app= Vue.createApp({
+data(){
+    return {
+        myName:"srini",
+        myAge:27,
+        myImage: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_1280.jpg",
+        randomNum: Math.random(),
+    }
+},
+methods:{
+
+
+}
+});
+
+app.mount('#assignment');
